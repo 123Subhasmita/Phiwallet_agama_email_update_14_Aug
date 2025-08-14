2a7577b774ac5b84f202d43207581b40e05b4f42
123Subhasmita <108878232+123Subhasmita@users.noreply.github.com>
2025-08-14 16:35:15 +0530

    refactor: update EmailOtpAr.groovy

--- projects/update-user-email/lib/org/gluu/agama/smtp/EmailOtpAr.groovy
+++ projects/update-user-email/lib/org/gluu/agama/smtp/EmailOtpAr.groovy
@@ -54,16 +54,24 @@ class EmailOtpAr {
                 <table role="presentation" cellspacing="0" cellpadding="0" width="570" align="center" style="margin:0 auto;padding:0;text-align:center;">
                   <tbody>
                     <tr>
-                      <td style="padding:20px;font-size:12px;color:#666;">
+                      <td style="padding:20px;font-size:12px;color:#666; direction:ltr; text-align:left;">
                         <p style="margin:0 0 10px 0;font-size:14px;font-weight:bold;color:#565555;">
-                            <span dir="ltr">Follow us on:</span>
+                          Follow us on:
                         </p>
-
-                          <a href="https://www.facebook.com/PhiWallet" style="margin:0 5px;"><img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/facebook.png" style="height:20px;"></a>
-                          <a href="https://x.com/PhiWallet" style="margin:0 5px;"><img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/twitter.png" style="height:20px;"></a>
-                          <a href="https://www.instagram.com/phi.wallet" style="margin:0 5px;"><img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/instagram.png" style="height:20px;"></a>
-                          <a href="https://www.linkedin.com/company/phiwallet" style="margin:0 5px;"><img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/linkedin.png" style="height:20px;"></a>
-                        </p>
+            
+                        <a href="https://www.facebook.com/PhiWallet" style="margin:0 5px;">
+                          <img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/facebook.png" style="height:20px;">
+                        </a>
+                        <a href="https://x.com/PhiWallet" style="margin:0 5px;">
+                          <img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/twitter.png" style="height:20px;">
+                        </a>
+                        <a href="https://www.instagram.com/phi.wallet" style="margin:0 5px;">
+                          <img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/instagram.png" style="height:20px;">
+                        </a>
+                        <a href="https://www.linkedin.com/company/phiwallet" style="margin:0 5px;">
+                          <img src="https://storage.googleapis.com/mwapp_prod_bucket/social_icon_images/linkedin.png" style="height:20px;">
+                        </a>
+            
                         <p style="margin-top:10px;line-height:20px;color:#A8AAAF;font-size:12px;">
                           Phi Wallet Unipessoal LDA<br>
                           Avenida da Liberdade 262 R/C<br>
@@ -76,6 +84,7 @@ class EmailOtpAr {
                 </table>
               </td>
             </tr>
+
 
           </tbody>
         </table>
